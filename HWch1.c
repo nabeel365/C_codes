97f4a462f11e1c75b43636caa6a4dc8186d97168
nabeel365 <choudhurinabeel@gmail.com>
2024-05-03 18:58:53 +0530

    even/odd numbers

---
+++ HWch1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+
+// perimeter of a rectangle
+
+int main(){
+
+// int a;
+// int b;
+// printf("Enter the length of the rectangle");
+// scanf("%d", &a);
+// printf("Enter the Breath of the rectangle");
+// scanf("%d", &b);
+// int perimeter = 2 * (a + b);
+// printf("The perimeter of the rectangle is - %d" , perimeter);
+
+
+// i want to convert the int into float ...
+// 
+
+// scanf("%f", &perimeter);
+
+
+// printf("Perimeter in float - %f", perimeter);
+
+
+
+// show that if a number is divisible by 2 or not 
+
+int x;
+printf("Enter a number");
+scanf("%d", &x);
+printf("%d", x % 2 == 0);
+return 0;
+
+
+
+
+
+}
---
+++ helloWorld.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+//  - 
+
+// jhhjhbh
+// jhvjh
+// kjbb
+
+/*
+
+*/
+
+
+// int main() {
+//     printf("Hello World gvhgvh\n");
+//     return 0;
+// }
+
+
+
+// find the area of a sqr
+
+int main() {
+    int side;
+    printf("Enter the side of the square");
+    scanf("%d", & side);
+    int area = side * side;
+    printf("The area is %d", area  );
+}
